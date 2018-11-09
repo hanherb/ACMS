77c1e94ba97ae0d8fc353e869ddad2a73a041701
hanherb <dyo.pyon@gmail.com>
2018-11-09 22:02:14 +0700

    Separate route from app.js

---
+++ route.js
@@ -0,0 +1,135 @@
+var express = require('express');
+var session = require('express-session');
+var mongodb = require('mongodb');
+var mongo = require('./config/mongo-connect');
+var fs = require('fs');
+var router = express.Router();
+router.route('/').get(function (req, res) {
+    res.redirect('/index.html');
+});
+//mengambil data dari collection user untuk ditampilkan di dashboard.html
+router.route('/get-user').get(function (req, res) {
+    mongo.mongoUser("find", {}, function (response) {
+        res.json(response);
+    });
+});
+//--
+router.route('/register-user').get(function (req, res) {
+    var obj = {
+        email: req.query.email,
+        fullname: req.query.fullname,
+        password: req.query.password,
+        role: "user",
+        authority: {
+            "read": 0,
+            "create": 0,
+            "update": 0,
+            "delete": 0
+        }
+    };
+    mongo.mongoUser("insert-one", obj, function (response) {
+        res.json(response.insertedCount);
+    });
+});
+router.route('/login-user').get(function (req, res) {
+    var query = { email: req.query.email, password: req.query.password };
+    mongo.mongoUser("find-query", query, function (response) {
+        if (response[0]) {
+            req.session.email = response[0].email;
+            req.session.fullname = response[0].fullname;
+            req.session.role = response[0].role;
+            req.session.authority = response[0].authority;
+            res.json(response[0]);
+        }
+        else {
+            res.json("Login error");
+        }
+    });
+});
+router.route('/update-user-form').get(function (req, res) {
+    var query = { email: req.query.email };
+    mongo.mongoUser("find-query", query, function (response) {
+        res.json(response);
+    });
+});
+router.route('/delete-user-form').get(function (req, res) {
+    var query = { email: req.query.email };
+    mongo.mongoUser("find-query", query, function (response) {
+        res.json(response);
+    });
+});
+router.route('/create-user').get(function (req, res) {
+    var obj = {
+        email: req.query.email,
+        fullname: req.query.fullname,
+        password: req.query.password,
+        role: req.query.role,
+        authority: req.query.authority
+    };
+    console.log(obj.authority);
+    mongo.mongoUser("insert-one", obj, function (response) {
+        res.json(response.insertedCount);
+    });
+});
+router.route('/update-user').get(function (req, res) {
+    var query = [{ email: req.query.email }, { $set: { fullname: req.query.fullname, role: req.query.role, authority: req.query.authority } }];
+    mongo.mongoUser("update-one", query, function (response) {
+        res.json(response);
+    });
+});
+router.route('/delete-user').get(function (req, res) {
+    var query = { email: req.query.email };
+    mongo.mongoUser("delete-one", query, function (response) {
+        res.json(response);
+    });
+});
+router.route('/check-session').get(function (req, res) {
+    if (req.session.email) {
+        var query = { email: req.session.email };
+        mongo.mongoUser("session", query, function (response) {
+            if (response) {
+                req.session.email = response[0].email;
+                req.session.fullname = response[0].fullname;
+                req.session.role = response[0].role;
+                req.session.authority = response[0].authority;
+            }
+        });
+        res.json(req.session);
+    }
+    else {
+        res.json("no session");
+    }
+});
+router.route('/list-plugin').get(function (req, res) {
+    var folder = __dirname + '/plugin/';
+    var temp = [];
+    fs.readdir(folder, function (err, files) {
+        files.forEach(function (file) {
+            if (file.substr(-3) == '.js')
+                temp.push(file.slice(0, -3));
+        });
+        res.json(temp);
+    });
+});
+router.route('/get-plugin').get(function (req, res) {
+    var plugin = req.query.plugin;
+    var query = {};
+    mongo.mongoPlugin("find", query, function (response) {
+        res.json(response);
+    });
+});
+router.route('/add-plugin').get(function (req, res) {
+    var plugin = req.query.plugin;
+    for (var i = 0; i < plugin.name.length; i++) {
+        var query = [{ name: plugin.name[i] }, { $set: { name: plugin.name[i], status: plugin.status[i] } }, { upsert: true }];
+        mongo.mongoPlugin("update", query, function (response) {
+        });
+    }
+    res.json(1);
+});
+router.route('/logout').get(function (req, res) {
+    req.session.destroy(function () {
+        res.redirect('/');
+    });
+});
+module.exports = router;
