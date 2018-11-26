16052b8a8ca3995994af2bd8e70cf430a274ea62
hanherb <dyo.pyon@gmail.com>
2018-11-26 19:45:44 +0700

    Optimizing route function code
    API Authentication temporary disabled because of no session running in main application

--- route.ts
+++ route.ts
@@ -10,130 +10,34 @@ const rf = require('./src/route-function')
 
 const router = express.Router();
 
-router.route('/').get(function(req, res) {
-	res.redirect('http://localhost:3001/');
-});
+router.route('/').get(function(req, res) {res.redirect('http://localhost:3001/')});
 
-router.route('/get-user').get(function(req, res) {
-	mongo.mongoUser("find", {}, function(response) {
-		res.json(response);
-	});
-});
+router.route('/get-user').get(function(req, res) {rf.getUser(req, res)});
 
-router.route('/register-user').get(function(req, res) {
-	let obj = rf.registerUser(req);
-	mongo.mongoUser("insert-one", obj, function(response) {
-		res.json(response.insertedCount);
-	});
-});
+router.route('/register-user').get(function(req, res) {rf.registerUser(req, res)});
 
-router.route('/login-user').get(function(req, res) {
-	let query = {email: req.query.email, password: req.query.password};
-	mongo.mongoUser("find-query", query, function(response) {
-		if(response[0]) {
-			let token = rf.jwtSign(response[0]);
-	        res.cookie('jwtToken', token);
-			rf.assignSession(req, res, response[0]);
-			console.log(req.session);
-			res.json(response[0]);
-		}
-		else {
-			res.json("Login error");
-		}
-	});
-});
+router.route('/login-user').get(function(req, res) {rf.loginUser(req, res)});
 
-router.route('/create-user').get(function(req, res) {
-	let obj = rf.registerUser(req);
-	mongo.mongoUser("insert-one", obj, function(response) {
-		res.json(response.insertedCount);
-	});
-});
+router.route('/create-user').get(function(req, res) {rf.registerUser(req, res)});
 
-router.route('/update-user').get(function(req, res) {
-	let query = [{email: req.query.email}, {$set: {fullname: req.query.fullname, role: req.query.role, authority: req.query.authority}}];
-	mongo.mongoUser("update-one", query, function(response) {
-		res.json(response);
-	});
-});
+router.route('/update-user').get(function(req, res) {rf.updateUser(req, res)});
 
-router.route('/delete-user').get(function(req, res) {
-	let query = {email: req.query.email};
-	mongo.mongoUser("delete-one", query, function(response) {
-		res.json(response);
-	});
-});
+router.route('/delete-user').get(function(req, res) {rf.deleteUser(req, res)});
 
-router.route('/check-session').get(function(req, res) {
-	if(req.session.email) {
-		let query = {email: req.session.email};
-		mongo.mongoUser("session", query, function(response) {
-			if(response) {
-				rf.assignSession(req, res, response[0]);
-			}
-		});
-		console.log(req.session);
-		res.json(req.session);
-	}
-	else {
-		res.json("no session");
-	}
-});
+router.route('/check-session').get(function(req, res) {rf.checkSession(req, res)});
 
-router.route('/list-plugin').get(function(req, res) {
-	let folder = __dirname + '/plugin/';
-	let temp = [];
-	fs.readdir(folder, (err, files) => {
-		files.forEach(file => {
-	 		temp.push(file);
-  		});
-  		res.json(temp);
-	});
-});
+router.route('/list-plugin').get(function(req, res) {rf.listPlugin(req, res)});
 
-router.route('/add-plugin').get(function(req, res) {
-	let plugin = req.query.plugin;
-	for(let i = 0; i < plugin.name.length; i++) {
-		let query = [{name: plugin.name[i]}, {$set: {name: plugin.name[i], status: plugin.status[i]}}, {upsert: true}];
-		mongo.mongoPlugin("update", query, function(response) {
-			
-		});
-	}
-	res.json(1);
-});
+router.route('/add-plugin').get(function(req, res) {rf.addPlugin(req, res)});
 
-router.route('/list-blog').get(function(req, res) {
-	mongo.mongoBlog("find", {}, function(response) {
-		res.json(response);
-	});
-});
+router.route('/list-blog').get(function(req, res) {rf.listBlog(req, res)});
 
-router.route('/add-post').get(function(req, res) {
-	let obj = rf.addPost(req);
-	mongo.mongoBlog("insert-one", obj, function(response) {
-		res.json(response);
-	});
-});
+router.route('/add-post').get(function(req, res) {rf.addPost(req, res)});
 
-router.route('/update-post').get(function(req, res) {
-	let query = [{title: req.query.old}, {$set: {title: req.query.title, content: req.query.content, date: req.query.date, month: req.query.month, year: req.query.year}}];
-	mongo.mongoBlog("update-one", query, function(response) {
-		res.json(response);
-	});
-});
+router.route('/update-post').get(function(req, res) {rf.updatePost(req, res)});
 
-router.route('/delete-post').get(function(req, res) {
-	let query = {title: req.query.title};
-	mongo.mongoBlog("delete-one", query, function(response) {
-		res.json(response);
-	});
-});
+router.route('/delete-post').get(function(req, res) {rf.deletePost(req, res)});
 
-router.route('/logout').get(function(req, res) {
-	res.clearCookie("jwtToken");
-	req.session.destroy(function() {
-		res.redirect('/');
-	});
-});
+router.route('/logout').get(function(req, res) {rf.logout(req, res)});
 
 module.exports = router;
--- src/route-function.js
+++ src/route-function.js
@@ -1,34 +1,108 @@
 const jwt = require('jsonwebtoken');
+const mongodb = require('mongodb');
+const mongo = require('./mongo-connect');
 
-exports.assignSession = function(req, res, data) {
-	req.session.email = data
-	req.session.fullname = data.fullname;
-	req.session.role = data.role;
-	req.session.authority = data.authority;
-	req.session.save();
+exports.getUser = function(req, res) {
+	mongo.mongoUser("find", {}, function(response) {
+		res.json(response);
+	});
 }
 
-exports.jwtSign = function(data) {
-	var token = jwt.sign({user: data},
-    	'kuda',
-      	{expiresIn: '24h'}
-    );
-    return token;
-}
-
-exports.registerUser = function(req) {
-	var obj = {
+exports.registerUser = function(req, res) {
+	let obj = {
 		email: req.query.email,
 		fullname: req.query.fullname,
 		password: req.query.password,
 		role: req.query.role,
 		authority: req.query.authority
 	};
-	return obj;
+	mongo.mongoUser("insert-one", obj, function(response) {
+		res.json(response.insertedCount);
+	});
 }
 
-exports.addPost = function(req) {
-	var obj = {
+exports.loginUser = function(req, res) {
+	let query = {email: req.query.email, password: req.query.password};
+	mongo.mongoUser("find-query", query, function(response) {
+		if(response[0]) {
+			let token = jwt.sign({user: response[0]},
+		    	'kuda',
+		      	{expiresIn: '24h'}
+		    );
+	        res.cookie('jwtToken', token);
+			req.session.email = response[0].email
+			req.session.fullname = response[0].fullname;
+			req.session.role = response[0].role;
+			req.session.authority = response[0].authority;
+			console.log(req.session);
+			res.json(response[0]);
+		}
+		else {
+			res.json("Login error");
+		}
+	});
+}
+
+exports.updateUser = function(req, res) {
+	let query = [{email: req.query.email}, {$set: {fullname: req.query.fullname, role: req.query.role, authority: req.query.authority}}];
+	mongo.mongoUser("update-one", query, function(response) {
+		res.json(response);
+	});
+}
+
+exports.deleteUser = function(req, res) {
+	let query = {email: req.query.email};
+	mongo.mongoUser("delete-one", query, function(response) {
+		res.json(response);
+	});
+}
+
+exports.checkSession = function(req, res) {
+	if(req.session.email) {
+		let query = {email: req.session.email};
+		mongo.mongoUser("session", query, function(response) {
+			if(response) {
+				rf.assignSession(req, res, response[0]);
+			}
+		});
+		console.log(req.session);
+		res.json(req.session);
+	}
+	else {
+		res.json("no session");
+	}
+}
+
+exports.listPlugin = function(req, res) {
+	let folder = __dirname + '/../plugin/';
+	let temp = [];
+	fs.readdir(folder, (err, files) => {
+		files.forEach(file => {
+	 		temp.push(file);
+  		});
+  		res.json(temp);
+	});
+}
+
+exports.addPlugin = function(req, res) {
+	let plugin = req.query.plugin;
+	for(let i = 0; i < plugin.name.length; i++) {
+		let query = [{name: plugin.name[i]}, {$set: {name: plugin.name[i], status: plugin.status[i]}}, {upsert: true}];
+		mongo.mongoPlugin("update", query, function(response) {
+			
+		});
+	}
+	res.json(1);
+}
+
+exports.listBlog = function(req, res) {
+	mongo.mongoBlog("find", {}, function(response) {
+		res.json(response);
+	});
+}
+
+exports.addPost = function(req, res) {
+	let obj = {
 		title: req.query.title,
 		content: req.query.content,
 		date: req.query.date,
@@ -36,5 +110,28 @@ exports.addPost = function(req) {
 		year: req.query.year,
 		author: req.session.fullname
 	}
-	return obj;
+	mongo.mongoBlog("insert-one", obj, function(response) {
+		res.json(response);
+	});
+}
+
+exports.updatePost = function(req, res) {
+	let query = [{title: req.query.old}, {$set: {title: req.query.title, content: req.query.content, date: req.query.date, month: req.query.month, year: req.query.year}}];
+	mongo.mongoBlog("update-one", query, function(response) {
+		res.json(response);
+	});
+}
+
+exports.deletePost = function(req, res) {
+	let query = {title: req.query.title};
+	mongo.mongoBlog("delete-one", query, function(response) {
+		res.json(response);
+	});
+}
+
+exports.logout = function(req, res) {
+	res.clearCookie("jwtToken");
+	req.session.destroy(function() {
+		res.redirect('/');
+	});
 }
