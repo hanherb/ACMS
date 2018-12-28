d3c770c00315386ba4548bd47be7d8a1e11013da
hanherb <dyo.pyon@gmail.com>
2018-12-28 18:30:34 +0700

    permission

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.use(session({
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', address + ':3001');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE, navPlugin');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
