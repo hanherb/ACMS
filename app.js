e6b9365c6cd76bc34ef5c1183d83a3875a206841
hanherb <dyo.pyon@gmail.com>
2018-12-28 18:34:20 +0700

    use cors

--- app.js
+++ app.js
@@ -25,10 +25,12 @@ app.use(session({
   cookie: { secure: false }
 }))
 
+app.use(cors());
+
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', address + ':3001');
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE, navPlugin');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
